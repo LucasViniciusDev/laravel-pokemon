67a33166b0bfeec2c6d73983e5e9af271fef9def
Lucas Vinicius <67592894+LucasViniciusDev@users.noreply.github.com>
2020-08-14 12:16:12 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return Redirect::intended('https://documenter.getpostman.com/view/7272792/T1LPDSCF?version=latest#693bbf71-d257-459a-9ea9-7a0b4f7de935');
+});
 
 Route::prefix('pokemons')->group(function () {
     Route::get('/', 'Api\PokemonController@index')->name('api.pokemons');
